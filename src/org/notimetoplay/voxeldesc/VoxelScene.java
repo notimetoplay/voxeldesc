28fe707fbd3d9c9f6c14ebae7d37b015b184dbad
Felix <felix@plesoianu.ro>
2014-10-07 21:54:55 +0300

    Color support, various small improvements.

--- src/org/notimetoplay/voxeldesc/VoxelScene.java
+++ src/org/notimetoplay/voxeldesc/VoxelScene.java
@@ -2,6 +2,8 @@ package org.notimetoplay.voxeldesc;
 
 import java.util.TreeMap;
 import java.awt.Color;
+import java.io.Writer;
+import java.io.BufferedReader;
 
 public class VoxelScene {
 	private TreeMap<Point3D, Color> voxels = new TreeMap<Point3D, Color>();
@@ -10,16 +12,31 @@ public class VoxelScene {
 		return voxels;
 	}
 
-	private Color drawingColor = Color.BLACK;
+	private Color fillColor = Color.BLACK;
 	
-	public Color getDrawingColor() {
-		return drawingColor;
+	public Color getFillColor() {
+		return fillColor;
 	}
 	
-	public void setDrawingColor(final Color c) {
+	public void setFillColor(final Color c) {
 		if (c == null) throw new NullPointerException(
 			"Non-null color expected");
-		drawingColor = c;
+		fillColor = c;
+	}
+	
+	public VoxelScene color(final int r, final int g, final int b) {
+		fillColor = new Color(r, g, b);
+		return this;
+	}
+	
+	public VoxelScene color(final int rgb) {
+		fillColor = new Color(rgb);
+		return this;
+	}
+	
+	public VoxelScene color(final Color c) {
+		setFillColor(c);
+		return this;
 	}
 	
 	private final Point3D cursor = new Point3D(0, 0, 0);
@@ -42,24 +59,42 @@ public class VoxelScene {
 		return this;
 	}
 	
-	public VoxelScene box(final byte wx, final byte wy, final byte wz) {
-		mark.x = (byte) (cursor.x + wx);
-		mark.y = (byte) (cursor.y + wy);
-		mark.z = (byte) (cursor.z + wz);
-		box();
+	public VoxelScene fill(final byte wx, final byte wy, final byte wz) {
+		mark.x = (byte) (cursor.x + wx - 1);
+		mark.y = (byte) (cursor.y + wy - 1);
+		mark.z = (byte) (cursor.z + wz - 1);
+		fill();
 		return this;
 	}
 	
-	public VoxelScene box() {
+	public VoxelScene fill() {
 		for (byte z = cursor.z; z <= mark.z; z++) {
 			for (byte y = cursor.y; y <= mark.y; y++) {
 				for (byte x = cursor.x; x <= mark.x; x++) {
 					voxels.put(
 						new Point3D(x, y, z),
-						drawingColor);
+						fillColor);
 				}
 			}
 		}
 		return this;
+	}
+	
+	public VoxelScene dot(final byte x, final byte y, final byte z) {
+		voxels.put(new Point3D(x, y, z), fillColor);
+		return this;
+	}
+	
+	public String toString() {
+		return String.format(
+			"Scene: %d voxels OK", voxels.size());
+	}
+	
+	public void serialize(Writer out) {
+		
+	}
+	
+	public void deserialize(BufferedReader in) {
+		
 	}
 }
