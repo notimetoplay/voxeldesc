46b7db7207b21f6fdb545d10c82604f73e2cc3d4
Felix <felix@plesoianu.ro>
2014-10-23 21:02:07 +0300

    Create hollow-recolor-paste.js

---
+++ samples/hollow-recolor-paste.js
@@ -0,0 +1,5 @@
+camera.zoomIn(3);
+scene.color(255, 0, 0, 128).from(-10, -10, -10).to(10, 0, 10).fill();
+scene.from(0, 0, 0).hollow(8);
+scene.color(blue).from(-10, -10, -10).to(10, 0, 10).recolor();
+scene.from(-8, -2, -8).paste();
